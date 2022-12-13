c6934815e0e299bad8b21114ec481196836ad9a6
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-13 13:16:27 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # devise_scope :user do
+  #   root to: 'devise/sessions#new'
+  # end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods, only: [:index, :show, :new, :create, :destory]
+  resources :recipes, only: [:index, :show, :new, :create, :destory]
+  resources :recipe_foods, only: [:create, :destroy]
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
+
 end
